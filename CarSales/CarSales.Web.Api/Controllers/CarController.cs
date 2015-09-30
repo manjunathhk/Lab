c3ff19bd0d8931f411134c55c0f7296dacda7f57
Manjunath HK <manjunathhk@gmail.com>
2015-09-30 16:58:22 +0530

    CarSales project added.
    This project can be used for various learning of the web techs.

---
+++ CarSales/CarSales.Web.Api/Controllers/CarController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using CarSales.Web.Api.Models;
+
+namespace CarSales.Web.Api.Controllers
+{
+    //[Route("api/car/")]
+    //[RoutePrefix("car")]
+    public class CarController : ApiController
+    {
+        private readonly IList<Car> cars;
+        public CarController()
+        {
+            cars = new List<Car>
+            {
+                new Car {Brand = "Hyundai", Model = "i10", MaxSpeed = 160, Torque = 10, Variant = "Era"},
+                new Car {Brand = "Hyundai", Model = "i10", MaxSpeed = 160, Torque = 10, Variant = "Magna"},
+                new Car {Brand = "Hyundai", Model = "i10", MaxSpeed = 160, Torque = 10, Variant = "Sportz"},
+                new Car {Brand = "Hyundai", Model = "i10", MaxSpeed = 160, Torque = 10, Variant = "Asta"},
+                new Car {Brand = "Hyundai", Model = "i20", MaxSpeed = 160, Torque = 10, Variant = "Era"},
+                new Car {Brand = "Hyundai", Model = "i20", MaxSpeed = 160, Torque = 10, Variant = "Magna"},
+                new Car {Brand = "Hyundai", Model = "i20", MaxSpeed = 160, Torque = 10, Variant = "Sportz"},
+                new Car {Brand = "Hyundai", Model = "i20", MaxSpeed = 160, Torque = 10, Variant = "Asta"},
+                new Car {Brand = "Maruthi", Model = "Ertiga", MaxSpeed = 140, Torque = 8, Variant = "LXi"},
+                new Car {Brand = "Maruthi", Model = "Ertiga", MaxSpeed = 140, Torque = 8, Variant = "VXi"},
+                new Car {Brand = "Maruthi", Model = "Ertiga", MaxSpeed = 140, Torque = 8, Variant = "ZXi"}
+            };
+        }
+        // GET: api/Car
+        public IList<Car> Get()
+        {
+            return cars;
+        }
+
+        // GET: api/Car/5
+        [Route("api/car/{brand}")]
+        //[Route("{brand}")]
+        public IList<Car> Get(string brand)
+        {
+            IList<Car> carList = (from car in cars
+                where car.Brand.Equals(brand, StringComparison.InvariantCultureIgnoreCase)
+                select car).ToList();
+
+            return carList;
+        }
+
+        [Route("api/car/{brand}/{model}")]
+        //[Route("{brand}/{model}")]
+        public IList<Car> Get(string brand, string model)
+        {
+            IList<Car> carList = (from car in cars
+                                  where car.Brand.Equals(brand, StringComparison.InvariantCultureIgnoreCase) && car.Model.Equals(model, StringComparison.InvariantCultureIgnoreCase)
+                                  select car).ToList();
+
+            return carList;
+        }
+
+        // POST: api/Car
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Car/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Car/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CarSales/CarSales.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarSales.Web.Api.WebApiApplication" Language="C#" %>
